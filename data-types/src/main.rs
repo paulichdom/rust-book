079e8bab74dbaea916912aa2d5786050337e05d3
Domagoj <paulic.domagoj@gmail.com>
2024-01-18 09:12:13 +0100

    explore data types (3.2)

---
+++ data-types/src/main.rs
@@ -0,0 +1,25 @@
+fn main() {
+    let tup = (500, 6.4, 1);
+
+    let (_x, y, _z) = tup;
+
+    println!("The value of y is: {y}");
+
+    let x: (i32, f64, u8) = (500, 6.4, 1);
+
+    let five_hundred = x.0;
+
+    let six_point_four = x.1;
+
+    let one = x.2;
+
+    println!("Accessed values -> {five_hundred}, {six_point_four} and {one}");
+
+    let _months = ["January", "February", "March", "April", "May", "June", "July",
+              "August", "September", "October", "November", "December"];
+
+    let _a: [i32; 5] = [1, 2, 3, 4, 5];
+
+    let _b = [3,5];
+
+}
