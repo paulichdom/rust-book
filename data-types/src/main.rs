a2594883946803c7734fcd3bd4a05f5e350df88f
Domagoj <paulic.domagoj@gmail.com>
2024-01-18 09:19:27 +0100

    answer data types questions

--- data-types/src/main.rs
+++ data-types/src/main.rs
@@ -3,7 +3,7 @@ fn main() {
 
     let (_x, y, _z) = tup;
 
-    println!("The value of y is: {y}");
+    // println!("The value of y is: {y}");
 
     let x: (i32, f64, u8) = (500, 6.4, 1);
 
@@ -13,7 +13,7 @@ fn main() {
 
     let one = x.2;
 
-    println!("Accessed values -> {five_hundred}, {six_point_four} and {one}");
+    // println!("Accessed values -> {five_hundred}, {six_point_four} and {one}");
 
     let _months = ["January", "February", "March", "April", "May", "June", "July",
               "August", "September", "October", "November", "December"];
@@ -22,4 +22,13 @@ fn main() {
 
     let _b = [3,5];
 
+    // Q1: 
+    /* let message = "The temperature today is:";
+    let x = [message, 100];
+    println!("{} {}", x[0], x[1]); */
+    
+    // Q2:
+    let t = ([1; 2], [3; 4]);
+    let (a, b) = t;
+    println!("{}", a[0] + t.1[0]); 
 }
