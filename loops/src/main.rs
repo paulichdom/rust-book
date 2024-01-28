7d7e5afa3fdeb98904aef0e54e19a7a3748118f6
Domagoj <paulic.domagoj@gmail.com>
2024-01-28 11:22:44 +0100

    explore loops

---
+++ loops/src/main.rs
@@ -0,0 +1,77 @@
+fn main() {
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    //println!("The result is {result}");
+    //nested_loop()
+    //liftoff();
+    //collect_elements();
+    //for_elements();
+    countdown();
+}
+
+fn nested_loop() {
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+        count += 1;
+    }
+    println!("End count = {count}");
+}
+
+fn liftoff() {
+    let mut number  = 3;
+
+    while number != 0 {
+        println!("{number}!");
+
+        number -= 1;
+    }
+
+    println!("LIFTOFF!!!")
+}
+
+fn collect_elements() {
+    let a = [10, 20, 30, 40, 50];
+    let mut index = 0;
+
+    while index < 5 {
+        println!("the value is {}", a[index]);
+
+        index += 1;
+    }
+}
+
+fn for_elements() {
+    let a = [10, 20, 30, 40, 50];
+
+    for element in a {
+        println!("the value is {element}");
+    }
+}
+
+fn countdown() {
+    for number in (1..5).rev() {
+        println!("{number}!")
+    }
+    println!("LIFTOFF!!!");
+}
